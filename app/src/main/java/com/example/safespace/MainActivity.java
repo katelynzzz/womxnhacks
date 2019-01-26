8c2758050071eac0cfa227cd0fa94aad9021b22a
Kara <karatsuh@gmail.com>
2019-01-26 14:15:19 -0800

    updoot

--- app/src/main/java/com/example/safespace/MainActivity.java
+++ app/src/main/java/com/example/safespace/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,5 +49,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void login (View view){
+        TextView showCountTextView = (TextView)
+                findViewById(R.id.textView);
     }
 }
