ca32afd6916c3cf93f767ac58dafa92e204c5d9d
karatsuh <karatsuh@gmail.com>
2019-01-26 15:44:58 -0800

    TEleporation achieved

--- app/src/main/java/com/example/safespace/MainActivity.java
+++ app/src/main/java/com/example/safespace/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.safespace;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -49,5 +50,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void login( View view){
+        Intent login = new Intent(this, ChooseOption.class);
+        startActivity(login);
     }
 }
