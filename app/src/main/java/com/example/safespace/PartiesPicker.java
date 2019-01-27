1aedf63c5d226a14f400b40edc4ff94746949d68
Katelyn Zhang <43655384+katelynzzz@users.noreply.github.com>
2019-01-26 16:51:16 -0800

    Button connecting

--- app/src/main/java/com/example/safespace/PartiesPicker.java
+++ app/src/main/java/com/example/safespace/PartiesPicker.java
@@ -1,5 +1,6 @@
 package com.example.safespace;
 
+import android.widget.ListView;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,12 +10,19 @@ import android.view.View;
 
 public class PartiesPicker extends AppCompatActivity {
 
+    ListView lst;
+    String[] addresses = {"123 Rainbow Dr.", "456 Brick Rd.", "1010 Main Ave.", "999 Random Rd.", "789 1st St."};
+    String[] ratings = {"4.1/5.0", "5.0/5.0", "2.2/5.0", "4.6/5/0", "3.6/5.0"};
+    Integer[] imgid = {R.drawable.house1, R.drawable.house2, R.drawable.house3, R.drawable.house4, R.drawable.house5};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parties_picker);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        lst = findViewById(R.id.list);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
